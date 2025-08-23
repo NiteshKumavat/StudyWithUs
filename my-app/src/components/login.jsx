ede7bc218ad03b180bb5a863b8434e08db45be68
Nitesh Kumavat <niteshkumavat32@gmail.com>
2025-08-23 23:20:41 +0530

    adding the login title

--- my-app/src/components/login.jsx
+++ my-app/src/components/login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'; // <-- import navigate
 import "./login.css";
 
 const AuthForm = () => {
+  document.title = "Login/Register - MyApp";
   const navigate = useNavigate("/dashboard"); 
   const [isLogin, setIsLogin] = useState(true);
   const [formData, setFormData] = useState({
